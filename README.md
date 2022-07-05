050db09dfa8b3d2ca0d2a6ffa9268b9d5cad2e3d
Veena Ram <veenaram92@gmail.com>
2022-07-05 12:43:09 -0400

    Add-awsdk-emf-binary-to-package

---
+++ README.md
@@ -0,0 +1,3 @@
+# PocPackage
+
+A description of this package.
---
+++ Sources/PocPackage/enterprise-mobile-framework.xcframework/ios-arm64/enterprise_mobile_framework.framework/Headers/enterprise_mobile_framework.h
@@ -0,0 +1,18 @@
+//
+//  enterprise_mobile_framework.h
+//  enterprise-mobile-framework
+//
+//  Created by Yue Shi on 29/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for enterprise_mobile_framework.
+FOUNDATION_EXPORT double enterprise_mobile_frameworkVersionNumber;
+
+//! Project version string for enterprise_mobile_framework.
+FOUNDATION_EXPORT const unsigned char enterprise_mobile_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <enterprise_mobile_framework/PublicHeader.h>
+
+
